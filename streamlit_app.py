b26bd4378fef3143f0dea8c88a5da18d205834c6
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-17 01:09:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ streamlit.header("Query SF Table!")
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load list")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:") 
 streamlit.text(my_data_row)
