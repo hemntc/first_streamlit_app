9f640e09c95150974e900384dd3b8eec8d10022c
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-16 20:52:38 +0530

    Update streamlit_app.py
    user entered api response changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,25 +35,25 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the result to screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 # New section to display new fruityvice api response
 streamlit.header("New Fruityvice Fruit Advice!")
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
 
 # take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output the result to screen as a table
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 
 # New section to display new fruityvice api response as per user entered fruit
@@ -64,5 +64,4 @@ streamlit.write('The user entered fruit : ', fruit_choice)
 
 import requests
 fruityvice_response_new = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
 
