a2ea20bc273a0e7ecfcfb8aedc6211195770b390
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-17 01:31:12 +0530

    Update streamlit_app.py
    stop for troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -111,6 +111,9 @@ streamlit.dataframe(my_data_row)
 # Add a text box to get fruit name from user
 streamlit.header("Query SF Table!")
 
+# don't run anything past here while we troubleshoot 
+streamlit.stop ()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
