8feda0178a3068d4b09769ded25a184478f064f0
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-17 01:43:58 +0530

    Update streamlit_app.py
    try except with if else

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,39 +34,23 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-# New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-
-# take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the result to screen as a table
-streamlit.dataframe(fruityvice_normalized)
-
-# New section to display new fruityvice api response
-streamlit.header("New Fruityvice Fruit Advice!")
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + 'kiwi')
-
-# take the json version of the response and normalise it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output the result to screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 # New section to display new fruityvice api response as per user entered fruit
-streamlit.header("User entered Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'apple')
+streamlit.header("Fruityvice Fruit Advice!")
+try
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+if not fruit_choice:
+  streamlit.error('Please select a fruit to get information')
+else:
 streamlit.write('The user entered fruit : ', fruit_choice)
 
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+streamlit.error()
 
 streamlit.header("Snowflake connection and other activities started !!!!")
 
