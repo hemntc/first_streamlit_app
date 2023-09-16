f31b348c15b6c9e791937e7e510c4295f4237f03
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-17 01:46:10 +0530

    Update streamlit_app.py
    try changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,19 +38,18 @@ streamlit.dataframe(fruits_to_show)
 # New section to display new fruityvice api response as per user entered fruit
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
 if not fruit_choice:
   streamlit.error('Please select a fruit to get information')
 else:
-streamlit.write('The user entered fruit : ', fruit_choice)
+  streamlit.write('The user entered fruit : ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-streamlit.error()
+  streamlit.error()
 
 streamlit.header("Snowflake connection and other activities started !!!!")
 
