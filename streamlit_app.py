0b62d2b02425db1ec327ebf6f60cb752cfdcd158
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-17 02:22:21 +0530

    Update streamlit_app.py
    title change

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ streamlit.header("Snowflake connection and other activities started !!!!")
 
 # Add a text box to get fruit name from user
 streamlit.header("Query SF Table!")
-streamlit.header("The fruit load list contains:") 
+streamlit.header("View our Fruit List - Add your Favourites !!") 
 
 
 # Snowflake related functions
@@ -71,7 +71,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
 
 # Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close()
