145800b6d462523de28d56a5c85d1b8dba0d045e
hemntc <144562757+hemntc@users.noreply.github.com>
2023-09-16 23:23:18 +0530

    Update streamlit_app.py
    api response changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-#import requests
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
